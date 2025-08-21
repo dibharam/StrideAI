0ed8c5378e831be8157667cbb5256cd2818ffe36
dibharam <di.bharam@gmail.com>
2025-08-22 00:58:05 +0100

    change home

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,8 +11,10 @@ export default async function Page() {
 
   return (
     <main style={{ padding: 24 }}>
-      <h1>StrideAI</h1>
-      <p>Next.js app is running ✅</p>
+      <h1>Name of App which I cant say yet</h1>
+      <p>Next.js app is running along with my backend :)</p>
+      <input placeholder="Enter Goal"></input>
+      <button>submit</button>
       <div style={{ marginTop: 16 }}>
         <h2>Backend health</h2>
         <pre>{JSON.stringify(health ?? { error: "API not reachable" }, null, 2)}</pre>
